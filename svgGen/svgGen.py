edaf550eca249c69a38427177fd89000d2fefb0a
ReneMuhl <r.muhl@gmx.net>
2013-05-16 00:17:18 +0200

    deleted:    genSVG1.svg 	deleted:    genSVG2.svg 	deleted:    genSVG3.svg 	deleted:    genSVG4.svg 	deleted:    genSVG5.svg 	new file:   output/2013-05-16 00:13:07.162247.svg 	new file:   output/2013-05-16 00:13:08.557389.svg 	new file:   output/2013-05-16 00:13:08.929319.svg 	new file:   output/2013-05-16 00:13:11.324916.svg 	new file:   output/2013-05-16 00:13:12.144325.svg 	new file:   output/2013-05-16 00:13:28.510992.svg 	new file:   output/2013-05-16 00:13:29.730976.svg 	new file:   output/2013-05-16 00:13:33.723766.svg 	renamed:    plainSVG2.svg -> output/2013-05-16 00:13:37.378046.svg 	new file:   output/2013-05-16 00:13:37.994064.svg 	new file:   output/2013-05-16 00:15:07.782678.svg 	new file:   output/2013-05-16 00:15:08.188640.svg 	new file:   output/2013-05-16 00:15:08.234850.svg 	new file:   output/2013-05-16 00:15:08.691243.svg 	new file:   output/2013-05-16 00:15:09.787260.svg 	new file:   output/2013-05-16 00:16:57.617517.svg 	new file:   output/2013-05-16 00:16:57.650390.svg 	new file:   output/2013-05-16 00:16:58.485755.svg 	new file:   output/2013-05-16 00:16:59.529474.svg 	new file:   output/2013-05-16 00:17:02.516384.svg 	modified:   plainSVG2.svg 	modified:   plainSVG5.svg 	modified:   svgGen.py

--- svgGen/svgGen.py
+++ svgGen/svgGen.py
@@ -15,7 +15,7 @@
 	# create structure with methods, commandline arguments like input, number - really necessary?
 	## of SVGs to be generated
 	#http://de.wikipedia.org/wiki/Gau%C3%9Fsche_Trapezformel
-
+	#https://code.google.com/p/svgfig/wiki/HowToInstall
 
 import svgfig  		#parse SVG
 import random 		#random numbers gen
@@ -23,22 +23,28 @@ import random 		#random numbers gen
 #import copy		#copy Objects
 #from sets import set 
 import ast
+import datetime
+from os import *	#system function like create folder
 
 
 ####################################
 #########params
 ####################################
 
-SVGInput_FileName="plainSVG5.svg"
-SVGOutput_FileNameTemplate="genSVG"
-numImages=15
-substitutionPercent=.6				#specifies how many Coord be replaced
+SVGInput_FileName="plainSVG2.svg"
+#SVGOutput_FileNameTemplate="genSVG"
+numImages=5
+substitutionPercent=.4				#specifies how many Coord be replaced
 
-xstart=120
-xstop=680
-ystart=120
-ystop=980
+#DIN A4:(744,1052)
+imageSizeX=744
+imageSizeY=1052
 
+
+minimumAreaPercentX=.3
+minimumAreaPercentY=.3
+#PercentPointsInQuadrants=.4
+pointsMustBeInQuadrants=4
 
 ####################################
 #########functions
@@ -93,7 +99,7 @@ def getRandomCoords():
 	rand2=random.uniform(ystart,ystop)
 	#validate coords with a seperate function/strategy
 	#print "rand1 rand2: ", rand1, rand2, "\n"
-	randCoords=str(rand1) + "," + str(rand2)
+	randCoords=unicode(rand1) + "," + unicode(rand2)
 	return randCoords
 
 
@@ -129,12 +135,14 @@ def transform_d_List_toPathD_AndGet(d_List):
 	return newPathD
 
 
-def modifySVGAndSave(newPathD,SVGobj,currentImg):
+def modifySVGAndSave(newPathD,SVGobj):
 	try:
 		SVGobj[2,0,u'd']=newPathD
 	except IndexError:
 		SVGobj[2,u'd']=newPathD
-	outputFileName=SVGOutput_FileNameTemplate+str(currentImg)+".svg"
+		now = datetime.datetime.now()
+
+	outputFileName="./output/"+str(now)+".svg"
 	print outputFileName,"\n========================\n"
 	SVGobj.save(outputFileName)
 
@@ -162,13 +170,13 @@ def normalizeSVG(d_List):
 			#print "testCoord:", testCoord
 			coord1=ast.literal_eval(testCoord[0])
 			if coord1 < 0:
-				#print "neg:",coord1
+				#print "neg:",coordX
 				coord1=-coord1
 			coord2=ast.literal_eval(testCoord[1])
 			if coord2 < 0:
 				#print "neg:",coord2
 				coord2=-coord2
-			d_List[index]=str(coord1) + "," + str(coord2)
+			d_List[index]=str(coordX) + "," + str(coord2)
 	newPathD = transform_d_List_toPathD_AndGet(d_List)
 	
 	try:
@@ -178,51 +186,125 @@ def normalizeSVG(d_List):
 	SVGobj.save(SVGInput_FileName)
 
 
+#Test whether the image braced a minimum area size.
+#explanation....
+def areaTest(d_List):
+	testLeftBelow=False
+	testLeftTop=False
+	testRightTop=False
+	testRightBelow=False
+
+	testSuccess=False
+	pointsInQuadrants=0
+	for index in range(0,len(d_List)):
+		if not d_List[index][0].isalpha():
+			print "num:",d_List[index]
+			testCoord = unicode.split(d_List[index],",")
+			coordX=ast.literal_eval(testCoord[0])
+			coordY=ast.literal_eval(testCoord[1])
+			if not testLeftBelow and (coordX < X_leftLimit and coordY < Y_belowLimit):
+				testLeftBelow=True
+				pointsInQuadrants+=1
+			if not testLeftTop and (coordX < X_leftLimit and coordY > Y_topLimit):
+				testLeftTop=True
+				pointsInQuadrants+=1
+			if not testRightTop and (coordX > X_rightLimit and coordY > Y_topLimit):
+				testRightTop=True
+				pointsInQuadrants+=1
+			if not testRightBelow and (coordX > X_rightLimit and coordY < Y_belowLimit):
+				testRightBelow=True
+				pointsInQuadrants+=1
+
+		print "pointsInQuadrants:",pointsInQuadrants
+		if pointsInQuadrants >= pointsMustBeInQuadrants:
+			testSuccess=True
+			break
+
+	return testSuccess
+
+
 
 ####################################
 #########main program
 ####################################
 
-SVGobj=loadSVGandGetXML(SVGInput_FileName)
-#print "SVGobj:",SVGobj
+#ranges of randomCoords
+# randomX (xstart,xstop)
+# randomY (ystart,ystop)
+xstart=imageSizeX*0.05
+print xstart
+xstop=imageSizeX*0.95
+print xstop
 
-d_List=getPathD_asList(SVGobj)
-#print "d_List:",d_List
-
-normalizeSVG(d_List)
-
-numCoords=getNumberOfCoords(d_List)
-print "numCoords:",numCoords
+ystart=imageSizeY*0.05
+print ystart
+ystop=imageSizeX*0.95
+print ystop
 
 
-numSubstitutions=int(numCoords*substitutionPercent)
-print "numSubstitutions:",numSubstitutions
+#Calculate the limits of the test area
+X_rightLimit=imageSizeX*(minimumAreaPercentX+(1-minimumAreaPercentX)/2)
+print X_rightLimit
+X_leftLimit=imageSizeX*(1-minimumAreaPercentX)/2
+print X_leftLimit
+
+Y_topLimit=imageSizeY*(minimumAreaPercentY+(1-minimumAreaPercentY)/2)
+print Y_topLimit
+Y_belowLimit=imageSizeY*(1-minimumAreaPercentY)/2
+print Y_belowLimit
+
+
 
 
 for currentImg in range(1,numImages+1):	
-	indexList = []
-	for coordNum in range(0,numSubstitutions):
-		
-		randIndex=getRandomIndex(numCoords)
-		#print "randIndex",coordNum,": ",randIndex
 
-		#idea: test if Coords differnt enough from other images
-		##newCoordsOK=false
-		## do{
-		##	randCoords=getRandomCoords()
-		##	newCoordsOK=validateCoords(randCoords)		#concate Coords later?
-		##	}while(newCoordsOK==true)
 
-		
-		randCoords=getRandomCoords()
-		print "randCoords",coordNum,":" ,randCoords,"\n"
+	testSuccess=False
+	while not testSuccess:
+		indexList = []
+		SVGobj=loadSVGandGetXML(SVGInput_FileName)
+		#print "SVGobj:",SVGobj
 
-		#print_d_List(d_List)
-		d_List = substituteCoordsAndGet_d_List(d_List,randIndex,randCoords)
-		#print_d_List(d_List)
+		d_List=getPathD_asList(SVGobj)
+		#print "d_List:",d_List
 
-		newPathD = transform_d_List_toPathD_AndGet(d_List)
-		#print newPathD
+		#normalizeSVG(d_List)
 
-	modifySVGAndSave(newPathD,SVGobj,currentImg)
+		numCoords=getNumberOfCoords(d_List)
+		print "numCoords:",numCoords
 
+		#pointsMustBeInQuadrants=PercentPointsInQuadrants*numCoords
+		print "pointsMustBeInQuadrants:",pointsMustBeInQuadrants
+
+		numSubstitutions=int(numCoords*substitutionPercent)
+		print "numSubstitutions:",numSubstitutions
+		if numSubstitutions == 0:
+			print "The number of points to be replaced is too small, please change the \"numSubstitutions\" parameter."
+		else:
+			for coordNum in range(0,numSubstitutions):
+				
+				randIndex=getRandomIndex(numCoords)
+				#print "randIndex",coordNum,": ",randIndex
+
+				#idea: test if Coords differnt enough from other images
+				##newCoordsOK=false
+				## do{
+				##	randCoords=getRandomCoords()
+				##	newCoordsOK=validateCoords(randCoords)		#concate Coords later?
+				##	}while(newCoordsOK==true)
+
+				
+				randCoords=getRandomCoords()
+				print "randCoords",coordNum,":" ,randCoords,"\n"
+
+				#print_d_List(d_List)
+				d_List = substituteCoordsAndGet_d_List(d_List,randIndex,randCoords)
+				#print_d_List(d_List)
+
+				newPathD = transform_d_List_toPathD_AndGet(d_List)
+				#print newPathD
+
+			testSuccess=areaTest(d_List)
+			print testSuccess
+			if testSuccess:
+				modifySVGAndSave(newPathD,SVGobj)
